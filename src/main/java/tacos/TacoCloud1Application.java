88f035d3d88066beeaf289ea45ee242be68067db
unknown <truongtansang61@gmail.com>
2023-03-31 06:49:33 +0700

    tacos

--- src/main/java/tacos/TacoCloud1Application.java
+++ src/main/java/tacos/TacoCloud1Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TacoCloudApplication {
+public class TacoCloud1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TacoCloudApplication.class, args);
+		SpringApplication.run(TacoCloud1Application.class, args);
 	}
 
 }
